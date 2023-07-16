de1d5ea950a3d54fbb5eed5feed4182721e99f74
FRANCIS <110781698+Foxxhaund@users.noreply.github.com>
2023-07-16 14:55:21 +0200

    nueva actualizacion

--- src/comopnents/Tabla/Tabla.jsx
+++ src/comopnents/Tabla/Tabla.jsx
@@ -1,40 +1,55 @@
 import { useOptionsContext } from "../../context/OptionsContext"
-import { Box, Grid } from "@mui/material";
-import { DataGrid } from '@mui/x-data-grid';
-import { buildLabels, buildLineValues, buildColores, buildValueLabels, buildDescription, buildTotales } from '../../services/buildTables';
+import { DataGrid,
+    GridToolbarContainer,
+    GridToolbarColumnsButton,
+    GridToolbarFilterButton,
+    GridToolbarExport,
+    GridToolbarDensitySelector, } from '@mui/x-data-grid';
+import { labelsX, valoresX, nombreX } from '../../services/buildTables';
 
+function CustomToolbar() {
+    return (
+      <GridToolbarContainer>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+        <GridToolbarDensitySelector />
+        <GridToolbarExport />
+      </GridToolbarContainer>
+    );
+  }
 export default function Tabla(){
     const {datos} = useOptionsContext()
     const {included} = datos
-    let tableData = []
-    //Valores para lineas
-    const LABELS = buildLabels(included) //datos para el eje x
-    const VALUES = buildLineValues(included)//datos para el eje y
-    const VALUE_LABELS = buildValueLabels(included)//etiquetas de lineas de grafica
-    //valores para barras
-    const COLORES = buildColores(included) //colores de la grafica
-    const TOTALES = buildTotales(included) //valores totales
-    const LASTUPDATE = buildDescription(included)
+    //let tableData = []
+    
+    //const COLORES = buildColores(included) 
+    const NOMBRES = nombreX(included)
+    const VALORES = valoresX(included)
+    const LX = labelsX(included)
 
-    TOTALES.map((t,index)=>{
-        tableData.push({
-            id: index,col1:LASTUPDATE[index],col2: VALUE_LABELS[index],col3: t
-        })
+    let cols = [
+        {field: 'Nombre', headerName: 'Nombre', width: 85, headerAlign: 'center'},
+    ]
+    LX.map((lx,index) =>{
+        cols.push({field: `col${index}`, headerName: lx, width: 85, headerAlign: 'center' })
     })
-    console.log(tableData)
-
-    const rows = tableData
-
-    const columns = [
-        { field: 'col1', headerName: 'ID', width: 150 },
-        { field: 'col2', headerName: 'ENERGIA', width: 150 },
-        { field: 'col3', headerName: 'VALOR', width: 150 },
-      ];
-
+    console.log(cols,"cols en tabla")
+    let rows = []
+    VALORES.map((V,index)=> {
+        let obj = {id: index, Nombre: NOMBRES[index]}
+        for(let i = 0; i < V.length; i++){
+            if(V[i]) obj[`col${i}`] =   V[i]
+            else obj[`"col${i}"`] = V[i] = ' '
+        } 
+        rows.push(obj)
+        obj = {}
+    })
+    
     return (
-        <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
+        <div style={{ height: 700, width: '100%' }}>
+            <p>dfgdfgerg</p>
+            <DataGrid rows={rows} columns={cols} slots={{toolbar: CustomToolbar}} />
         </div>
         )
 
-}
+ }
